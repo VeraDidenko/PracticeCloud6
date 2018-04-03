c5bf381e63c1fc4bbb512f99a66ac06686193925
Vera <verochka1998@gmail.com>
2018-04-03 13:56:42 +0300

    Send announcement to email

--- 00_Conference_Central/src/main/java/com/google/devrel/training/conference/servlet/SetAnnouncementServlet.java
+++ 00_Conference_Central/src/main/java/com/google/devrel/training/conference/servlet/SetAnnouncementServlet.java
@@ -4,6 +4,7 @@ import static com.google.devrel.training.conference.service.OfyService.ofy;
 
 import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.appengine.api.utils.SystemProperty;
 import com.google.common.base.Joiner;
 import com.google.devrel.training.conference.Constants;
 import com.google.devrel.training.conference.domain.Conference;
@@ -11,7 +12,16 @@ import com.google.devrel.training.conference.domain.Conference;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +34,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @SuppressWarnings("serial")
 public class SetAnnouncementServlet extends HttpServlet {
+	private static final Logger LOG = Logger.getLogger(
+			SetAnnouncementServlet.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -47,20 +59,36 @@ public class SetAnnouncementServlet extends HttpServlet {
                     "Last chance to attend! The following conferences are nearly sold out: ");
             Joiner joiner = Joiner.on(", ").skipNulls();
             announcementStringBuilder.append(joiner.join(conferenceNames));
+            String email = "elizthacher@gmail.com";
+            Properties props = new Properties();
+            Session session = Session.getDefaultInstance(props, null);
+            String body = announcementStringBuilder.toString();
+            try {
+                Message message = new MimeMessage(session);
+                InternetAddress from = new InternetAddress(
+                        String.format("noreply@%s.appspotmail.com",
+                                SystemProperty.applicationId.get()), "Conference Central");
+                message.setFrom(from);
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, ""));
+                message.setSubject("Last chance to attend!");
+                message.setText(body);
+                Transport.send(message);
+                // TODO
+                // Get the Memcache Service
+                MemcacheService memcacheService = MemcacheServiceFactory.getMemcacheService();
+                // TODO
+                // Put the announcement String in memcache,
+                // keyed by Constants.MEMCACHE_ANNOUNCEMENTS_KEY
+                memcacheService.put(Constants.MEMCACHE_ANNOUNCEMENTS_KEY, announcementStringBuilder);
+                // Set the response status to 204 which means
+                // the request was successful but there's no data to send back
+                // Browser stays on the same page if the get came from the browser
+                response.setStatus(204);
+            } catch (MessagingException e) {
+                LOG.log(Level.WARNING, String.format("Failed to send an mail to %s", email), e);
+                throw new RuntimeException(e);
+            }
 
-            // TODO
-            // Get the Memcache Service
-            MemcacheService memcacheService = MemcacheServiceFactory.getMemcacheService();
-
-            // TODO
-            // Put the announcement String in memcache,
-            // keyed by Constants.MEMCACHE_ANNOUNCEMENTS_KEY
-            memcacheService.put(Constants.MEMCACHE_ANNOUNCEMENTS_KEY, announcementStringBuilder);
         }
-
-        // Set the response status to 204 which means
-        // the request was successful but there's no data to send back
-        // Browser stays on the same page if the get came from the browser
-        response.setStatus(204);
     }
 }
